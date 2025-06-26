464ba2248ed202fe818e7ae08dec3e9f2811fce2
shohan11d@gmail.com <shohan11d@gmail.com>
2025-06-26 13:20:34 +0600

    header updated

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-10 ">
-      <div className="contaner flex h-14 items-center max-lg:px-5">
-        <a href="">
+      <div className="contaner flex h-14 items-center  max-lg:px-5">
+        <a className="lg:hidden flex-1 cursor-pointer z-2" href="" >
           <img src="/images/xora.svg" width={115} height={55} alt="Xora" />
         </a>
       </div>
